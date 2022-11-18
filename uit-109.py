1ba037d923500a9fce23c5eb346da6d80425f3f6
Towhidspixs <114253386+Towhidspixs@users.noreply.github.com>
2022-11-18 21:33:58 +0600

    Add files via upload

---
+++ uit-109.py
@@ -0,0 +1,294 @@
+# # # from calendar import day_abbr
+# # # from tkinter import MULTIPLE
+# # # from urllib.request import HTTPPasswordMgrWithDefaultRealm
+
+
+# # # print("hello world") #perentathis mean brackets
+# # # # print("don't") #commnets
+
+# # # '''How- its a multiple comments 
+# # # how 
+# # # how '''
+
+# # # # how
+# # # # how was the day
+
+# # # '''how was the day 
+
+# # # Its thgursday
+# # # hello'''
+
+# # # '''multiple 
+# # # multiple 
+# # # multiple '''
+
+# # # # single commnet
+# # # # single 
+
+# # # # variable= plate
+# # # # value= rice
+
+# # # a ="My" #lekha manei sting
+# # # b="name"
+# # # c="is Towhid"
+# # # number=55
+# # # price=89
+# # # print(a,b,c)
+
+# # # shirt_product_price = 500
+# # # print(shirt_product_price)
+
+# # # # keyword
+# # # '''print,if,elif,else,for,in,input,def,class,break'''
+# # # # num2=90
+# # # # print(num2)
+
+# # # print(type(a))
+# # # print(type(shirt_product_price))
+
+
+# # # # type janar jonno type 
+# # # # int ke chaile str e convert kora jay
+# # # product='99'
+# # # #product1=int(product)
+# # # #print(int(product1))
+# # # #print(type(int(product1)))
+# # # print(type(product))
+
+# # # #Product_name= shirt 
+# # # # Product_name1=int(shirt)
+# # # # print(type(Product_name1))
+ 
+# # # number=77
+# # # number_str=str(number)
+# # # print(number_str)
+# # # print(type(number_str))
+# # # print(type(number))
+
+# # # number=89.0
+# # # num= int(number)
+# # # print(num)
+# # # print(type(num))
+
+# # # # python operator
+# # # # Python Arithmetic Operators
+# # # num1=189
+# # # num2=46
+# # # num3= 6
+# # # ok=num1 - num2 + num1
+# # # ok1=str(ok)
+# # # print(ok1)
+# # # print(type(ok1)) 
+
+# # # print(num2*num3)
+
+# # # a = 23
+# # # b= 34
+# # # c = 22
+# # # d= 5
+# # # e = 10
+# # # Num1= a+b+c
+# # # print(Num1)
+
+# # # Num2=a+c-e
+# # # print(Num2)
+
+# # # Num3= e/d
+# # # print(int(Num3))
+
+# # # Num4= b//d
+# # # print(Num4)
+
+# # # Num5= b%e
+# # # print(Num5)
+
+# # # Num6= c/e
+# # # print(float(Num6))
+
+# # # Num7= 89
+# # # print(Num7)
+
+# # # #assignment operator
+# # # num1+=6
+# # # print(num)
+
+# # # x=1
+# # # x+=3
+# # # print(x)
+
+# # # y=10
+# # # y-=5
+# # # print(y)
+
+# # # g= 2
+# # # g*=2
+
+# # # print(g)
+
+# # # print(g)
+
+# # # Num8=20
+# # # Num8 &=5
+
+# # # print(Num8)     #32,16,8,4,2,1
+
+# # # Num9=10         #1010
+# # # Num9|=8         #1000
+
+# # # print(Num9)
+
+# # # # conditional statement
+# # from re import A
+
+
+# # Num10= 10
+# # Num11= 15
+# # Num12 = 30
+# # if Num10> Num11 and Num10 > Num12:
+# #     print(Num10)
+# # elif Num11>Num10 and Num11 > Num12:
+# #     print(Num11)
+# # else: 
+# #     print(Num12)
+    
+# a1=35
+# a2=44
+# a3=89
+# a4=65
+
+# if a1>a2 and a1>a3 and a1>a4:
+#     print(a1)
+# elif a2>a1 and a2>a3 and a2>a4:
+#     print(a2)
+# elif a3>a1 and a3>a2 and a3>a4:
+#     print(a3)
+# else:
+#     print(a4)
+
+# if a1==a2:
+#     print("Done")
+# else:
+#     print("Exit")
+    
+# if a3<a4:
+#     print("No")
+# elif a3==a4:
+#     print("Ok")
+# else:
+#     print('somthing going wrong')
+
+# # # SSC Grading System Marks	Points	Grade
+# # # SSC Grading System
+# # # Marks	Points	Grade
+# # # 80 – 100	5.00	A+
+# # # 70 – 79	4.00	A
+# # # 60 – 69	3.50	A-
+# # # 50 – 59	3.00	B
+# # # 40 – 49	2.00	C
+# # # 33 – 39	1.00	D
+# # # 0 – 32	0.00	F
+
+# # result = int(input("Enter Your number "))
+# # if 100<result or result<0:
+# #     print("invalid")
+# # elif 100 >= result >=80:
+# #     print("A+")
+# # elif 79 >= result >=70:
+# #     print("A")
+# # elif 69 >= result >=60:
+# #     print("A-")
+# # elif 59>= result >=50:
+# #     print("B")
+# # elif 49 >= result >=40:
+# #     print("C")
+# # elif 39 >= result >=33:
+# #     print("D")
+# # else:
+# #     print("Fail")
+
+# #floor division
+# # a=25
+# # b=4
+# # print(a//b)
+
+# # assignment operator
+# x=3
+# x+=10
+# print(x)
+
+# x=7
+# x/=2
+# print(float(x))
+
+# x=98
+# x%=5
+# print(x)
+
+
+# # for x in 'Banana':
+# #     print(x)
+    
+# Number=['1','2','3','4','5','6','7','8','9','11']
+# for u in Number:
+#     print(u)
+#     if u=='6':
+#        break
+            
+            
+#             #Q-1
+# '''Sentence gulo emon vabe make korte hobe jeno 'I' and "you" er sathe "knows" na ase
+# sudhu jeno "know ase
+# and "she" er sathe jeno "know" na ase, shudu jeno "knows" ase
+# '''
+# c1=['I','You','she']
+# c2=['know','knows']
+# c3=['Towhid']
+
+# for x in c1:
+# #     if x=='knows':
+# #         break
+#  for y in c2:
+# #     if y=='knows':
+# #         break
+#   for z in c3:
+# #     if z=='know':
+# #        pass
+
+#    print(x,y,z)
+
+
+# # Q-2
+# '''I=am, 
+#     you=are, 
+#       He/she=is hobe,,,
+
+# I don't equal is or are,
+#  you don't equal am or is
+#   He/she don't equal am or are
+
+# '''
+# s1=['I',"you",'He']
+# s2=['am','is','are']
+
+# for x in s1:
+
+#  for y in s2:
+#   print(x,y)
+  
+# # c1=[a='I',b='You',c='she']
+# # c2=[d='know',e='knows']
+# # c3=[f='Towhid']
+
+# # while 
+
+# #    print(x,y,z)
+
+i = 20
+while i < 30:
+  print(i)
+  if i == 25:
+    break
+  i += 1
+  
+        
+    
